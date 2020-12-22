ac9f564364cd60041c219550b9224b54d793ee21
Jen-Wei Liu <jwliunctu@users.noreply.github.com>
2020-12-22 23:01:52 +0800

    可以用繁體字搜尋簡體字短評

--- week04/MyDjango/movie/views.py
+++ week04/MyDjango/movie/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Comment
+from hanziconv import HanziConv
 
 # Create your views here.
 
@@ -9,5 +10,6 @@ def index(request):
 
 def movie(request):
     search = request.GET.get('q','') #取得網址q參數
+    search = HanziConv.toSimplified(search) #轉簡體
     data = Comment.objects.filter(content__contains=search,star__gte=4).exclude(star=0).order_by('-created_at').all()
     return render(request, 'movie.html', locals())
